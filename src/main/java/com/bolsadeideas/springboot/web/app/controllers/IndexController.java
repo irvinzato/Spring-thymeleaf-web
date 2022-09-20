1c49b8fb2c950ba7249a97cb0dd0a0436956f9fe
irvinzato <multizato@hotmail.com>
2022-09-20 15:35:16 -0500

    Añadi modelo Usuario, con nuevo metodo de controlador y vista

--- src/main/java/com/bolsadeideas/springboot/web/app/controllers/IndexController.java
+++ src/main/java/com/bolsadeideas/springboot/web/app/controllers/IndexController.java
@@ -5,6 +5,8 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import com.bolsadeideas.springboot.web.app.models.Usuario;
+
 @Controller
 @RequestMapping("/app") //Añado ruta de primer nivel "http://localhost:8080/app"
 public class IndexController {
@@ -13,7 +15,20 @@ public class IndexController {
 	@GetMapping({"/index", "/", "/home"})	//Puedo omitir "value". Si esta asociado a muchas vistas las pongo entre llaves
 	public String index(Model model) {
 		model.addAttribute("title", "Pase datos a la vista con Model, puedo usar ModelMap, Map, ModelAndView. Otras formas.");
-		return "index";		//Mismo nombre que tiene archivo html en carpeta "template"
+		return "index";		//Mismo nombre que tiene archivo html en carpeta "template" sin extensión
 	}
+	
+	@RequestMapping("/perfil")
+	public String pofile(Model model) {
+		Usuario user = new Usuario();
+		user.setName("Irving");
+		user.setLastName("Rivera");
+		
+		model.addAttribute("title", "Perfil de ".concat(user.getName()));
+		model.addAttribute("usuario", user);
+		return "perfil";
+	}
+	
+	
 	
 }
