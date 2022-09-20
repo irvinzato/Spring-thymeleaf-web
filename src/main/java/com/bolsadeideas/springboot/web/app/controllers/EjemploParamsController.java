b53412d7652d9fae1c0a351256bd56fcfe9d4fc2
irvinzato <multizato@hotmail.com>
2022-09-20 17:48:29 -0500

    Mande mas de un parametro de diferentes formas

--- src/main/java/com/bolsadeideas/springboot/web/app/controllers/EjemploParamsController.java
+++ src/main/java/com/bolsadeideas/springboot/web/app/controllers/EjemploParamsController.java
@@ -1,5 +1,7 @@
 package com.bolsadeideas.springboot.web.app.controllers;
 
+import javax.servlet.http.HttpServletRequest;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -20,6 +22,29 @@ public class EjemploParamsController {
 		model.addAttribute("resultado", "El parametro enviado es: " + texto);
 		
 		return "params/ver";	//Ruta en templates
+	}
+	
+	//Envio más de un parámetro
+	@GetMapping("/mix-params")	
+	public String mixParam(@RequestParam String saludo, @RequestParam Integer num, Model model) {
+		model.addAttribute("resultado", "El parametro enviado es: " + saludo + " el numero es: " + num);
+		
+		return "params/ver";
+	}
+	
+	//Otra forma pero sin anotaciones(menos elegante y moderno)
+	@GetMapping("/mix-params-request")	
+	public String mixParam(HttpServletRequest request, Model model) {
+		String saludo = request.getParameter("saludo");
+		Integer num = null;
+		try {
+			num = Integer.parseInt(request.getParameter("num"));
+		}catch(NumberFormatException e) {
+			num = 0;
+		}
+		model.addAttribute("resultado", "El parametro enviado es: " + saludo + " el numero es: " + num);
+		
+		return "params/ver";
 	}
 
 }
